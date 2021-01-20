0881a7d080872d94983567b144a9a027377c9573
RobertPujante <moralesso152842@gmail.com>
2021-01-20 10:06:15 +0800

    optimised login forms

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,12 @@
 feather.replace();
+
+function screenResolution(){
+    let resolution = document.getElementsByClassName('snippet-size');
+    
+    for (let i = 0; i < resolution.length; i++) {
+        resolution[i].setAttribute('width', window.screen.width * window.devicePixelRatio);
+        resolution[i].setAttribute('height', window.screen.height * window.devicePixelRatio);   
+    }
+}
+
+screenResolution();
